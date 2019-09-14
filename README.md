26e2ec58a11eb379ea0f0b49b9ef48a34518a00f
Ismael de Jesus <develian@Ismaels-iMac-Pro.local>
2019-09-14 16:01:56 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# github-user-stars-react-native
---
+++ src/pages/User/index.js
@@ -0,0 +1,122 @@
+import React, { Component } from 'react';
+//import { ActivityIndicator } from 'react-native';
+import PropTypes from 'prop-types';
+import api from '../../services/api';
+
+import {
+  Container,
+  Header,
+  Avatar,
+  Name,
+  Bio,
+  Stars,
+  Starred,
+  OwnerAvatar,
+  Info,
+  Title,
+  Author,
+  Loading,
+  EmptyInfo,
+} from './styles';
+
+export default class User extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: navigation.getParam('user').name,
+  });
+
+  static propTypes = {
+    navigation: PropTypes.shape({
+      getParam: PropTypes.func,
+      navigate: PropTypes.func,
+    }).isRequired,
+  };
+
+  state = {
+    stars: [],
+    page: 1,
+    loading: true,
+    refreshing: false,
+  };
+
+  async componentDidMount() {
+    this.loadStars();
+  }
+
+  loadStars = async (page = 1) => {
+    const { stars } = this.state;
+    const { navigation } = this.props;
+    const user = navigation.getParam('user');
+
+    const response = await api.get(`/users/${user.login}/starred`, {
+      params: { page },
+    });
+
+    this.setState({
+      stars: page >= 2 ? [...stars, ...response.data] : response.data,
+      page,
+      loading: false,
+      refreshing: false,
+    });
+  };
+
+  loadMore = () => {
+    const { page } = this.state;
+
+    const nextPage = page + 1;
+
+    this.loadStars(nextPage);
+  };
+
+  refreshList = () => {
+    this.setState({ refreshing: true, stars: [] }, this.loadStars);
+  };
+
+  handleNavigate = repository => {
+    const { navigation } = this.props;
+
+    navigation.navigate('Repository', { repository });
+  };
+
+  render() {
+    const { navigation } = this.props;
+    const { stars, loading, refreshing } = this.state;
+
+    const user = navigation.getParam('user');
+
+    return (
+      <Container>
+        <Header>
+          <Avatar source={{ uri: user.avatar }} />
+          <Name>{user.name}</Name>
+          <Bio>{user.bio}</Bio>
+        </Header>
+
+        {stars.length === 0 && !loading && !refreshing && (
+          <EmptyInfo>{'Sem favoritos'}</EmptyInfo>
+        )}
+
+        {loading ? (
+          <Loading />
+        ) : (
+          <Stars
+            onRefresh={this.refreshList}
+            refreshing={refreshing}
+            data={stars}
+            keyExtractor={star => String(star.id)}
+            onEndReached={this.loadMore}
+            onEndReachedThreashold={0.2}
+            renderItem={({ item }) => (
+              <Starred onPress={() => this.handleNavigate(item)}>
+                <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+                <Info>
+                  <Title>{item.name}</Title>
+                  <Author>{item.owner.login}</Author>
+                </Info>
+              </Starred>
+            )}
+          />
+        )}
+      </Container>
+    );
+  }
+}
